549fb19a5f8f22c207cc0313a3c1fbe04b3703ec
off.rkv <off.rkv@gmail.com>
2025-06-14 00:58:38 +0530

    data transfromation done

---
+++ src/components/data_transformation.py
@@ -0,0 +1,131 @@
+#Data transformation(Columns Transformation and Standard Scaler)
+
+import sys
+import os
+import pandas as pd
+import numpy as np
+
+from dataclasses import dataclass
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import IterativeImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.linear_model import BayesianRidge
+
+from src.exception import CustomException
+from src.logger import logging
+from src.components.data_ingestion import DataIngestion
+from src.utils import save_object
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_transformation_object(self):
+        '''This function is responsible for data transformation'''
+
+        ingestion=DataIngestion()
+        ingestion.initiate_data_ingestion()
+        df=ingestion.df
+
+        try:
+            numerical_columns=df.select_dtypes(exclude='O').columns.to_list() # list of all the numerical columns
+            categorical_columns=df.select_dtypes(include='O').columns.to_list() # list of all the categorical columns
+
+            #Use to pipeline to chain together multiple data transformation 
+            logging.info("Pipeline the data transformation steps")
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", IterativeImputer(
+                        estimator=BayesianRidge(),
+                        max_iter=10,
+                        skip_complete=True,
+                        random_state=0
+                    )),
+                    ("scaler", StandardScaler())
+                ]
+            )
+
+            cat_pipeline=Pipeline(
+                steps=[
+                    ('imputer',IterativeImputer(
+                        estimator=BayesianRidge(),
+                        max_iter=10,
+                        skip_complete=True,
+                        random_state=0
+                    )),
+                    ('one_hot_encoder',OneHotEncoder())
+                ]
+            )
+            logging.info("Numerical columns standard scaling completed")
+
+            logging.info("Categorical Columns encoding completed")
+
+            preprocessor=ColumnTransformer(
+                [
+                    ('num_pipeline',num_pipeline,numerical_columns),
+                    ('cat_pipeline',cat_pipeline,categorical_columns)
+                ]
+            )
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+            logging.info("Read train and test data completed")
+            logging.info('Obtaining preprocessing object')
+
+            preprocessing_obj=self.get_transformation_object()
+
+            target_column_name='Personality'
+
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+
+            target_feature_train_df = train_df[target_column_name]
+            target_feature_test_df = test_df[target_column_name]
+            
+            logging.info(
+                f'Applying preprocessor object on training dataframe and testing dataframe'
+            )
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            #concatenate
+            train_arr=np.c_[
+                input_feature_train_arr,np.array(target_feature_train_df)
+            ]
+
+            test_arr=np.c_[
+                input_feature_test_arr,np.array(target_feature_test_df)
+            ]
+
+            logging.info(f'Saved preprocessing object & Train shape -> {train_arr.shape} and Test_arr -> {test_arr.shape}.')
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )                        
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
